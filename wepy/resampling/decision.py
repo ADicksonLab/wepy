c859d0f18eece84bc16b0d37e3ac9c1aec68bd24
salotz <salotz@salotz.info>
2017-07-12 18:05:22 -0400

    removed missed merge conflicts and stuff from decision.py
    There was a merge conflict magit was not bringing up so I got rid of
    it in decision.
    
    THere is also a bunch of crap in decision I got rid of

--- wepy/resampling/decision.py
+++ wepy/resampling/decision.py
@@ -1,40 +1,10 @@
 from enum import Enum
 
 class Decision(Enum):
-<<<<<<< HEAD
     pass
 
 class CloneMergeDecision(Decision):
     NOTHING = 1
     CLONE = 2
     SQUASH = 3
-    MERGE = 4
-=======
-    NOTHING = 0
-    CLONE = 1
-    KEEP_MERGE = 2
-    SQUSH = 3
->>>>>>> master
-
-class DecisionModel(object):
-
-    def decide(self, novelties):
-        raise NotImplementedError
-
-
-class NoCloneMerge(DecisionModel):
-    """Example stub for a DecisionModel subclass, always returns a NOTHING
-    decision for all inputs."""
-
-    def decide(self, novelties):
-        return [Decision.NOTHING for i in novelties]
-
-class RandomCloneMerge(DecisionModel):
-
-    def __init__(self, seed):
-        raise NotImplementedError
-
-class WExplore2(DecisionModel):
-    # Nazanin Put your code here!!
-    pass
-
+    KEEP_MERGE = 4
