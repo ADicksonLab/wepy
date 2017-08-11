efedf48e1c7273bfbc2f6845c944cd3a693f488b
salotz <salotz@salotz.info>
2017-08-11 17:42:52 -0400

    working on TrajHDF5 and example using it

---
+++ prototypes/hdf5/traj_h5.py
@@ -0,0 +1,27 @@
+import mdtraj as mdj
+import numpy as np
+
+from wepy.hdf5 import TrajHDF5
+
+traj = mdj.load_dcd('test_traj.dcd', top="sEH_TPPU_system.pdb")
+topology = traj.topology
+time = traj.time
+# positions
+positions_q = [traj.openmm_positions(i) for i in range(traj.n_frames)]
+positions = np.array([e._value for e in positions_q])
+positions_unit = positions_q[0].unit.get_name()
+# box_vectors
+box_vectors_q = [traj.openmm_boxes(i) for i in range(traj.n_frames)]
+box_vectors = np.array([e._value for e in box_vectors_q])
+box_vectors_unit = box_vectors_q[0].unit.get_name()
+
+traj_h5 = TrajHDF5('test_traj.h5',
+                   topology=topology,
+                   positions=positions,
+                   time=time,
+                   box_vectors=box_vectors,
+                   positions_unit=positions_unit,
+                   time_unit='second',
+                   box_vectors_unit=box_vectors_unit)
+
+traj_h5.close()
--- wepy/hdf5.py
+++ wepy/hdf5.py
@@ -10,7 +10,15 @@ class TrajHDF5(object):
                  topology=None,
                  positions=None,
                  time=None,
-                 box_vectors=None
+                 box_vectors=None,
+                 velocities=None,
+                 positions_unit=None,
+                 time_unit=None,
+                 box_vectors_unit=None,
+                 velocities_unit=None,
+                 forces=None,
+                 parameters=None,
+                 observables=None
     ):
         """Initializes a TrajHDF5 object which is a format for storing
         trajectory data in and HDF5 format file which can be used on
@@ -39,7 +47,20 @@ class TrajHDF5(object):
             if topology is None:
                 raise ValueError("topology must be given for new files")
             else:
+                # set the units
+                self.positions_units = positions_unit
+                self.time_unit = time_unit
+                self.box_vectors_unit = box_vectors_unit
+                self.velocities_unit = velocities_unit
+                # use the setters to add the data
                 self.topology = topology
+                self.positions = positions
+                self.time = time
+                self.box_vectors = box_vectors
+                self.velocities = velocities
+                self.forces = forces
+                self.parameters = parameters
+                self.observables = observables
 
 
     @property
@@ -65,6 +86,62 @@ class TrajHDF5(object):
     @topology.setter
     def topology(self, topology):
         self._h5.create_dataset('topology', data=topology)
+
+    @property
+    def positions(self):
+        return self._h5['positions']
+
+    @positions.setter
+    def positions(self, positions):
+        self._h5.create_dataset('positions', data=positions)
+
+    @property
+    def time(self):
+        return self._h5['time']
+
+    @time.setter
+    def time(self, time):
+        self._h5.create_dataset('time', data=time)
+
+    @property
+    def box_vectors(self):
+        return self._h5['box_vectors']
+
+    @box_vectors.setter
+    def box_vectors(self, box_vectors):
+        self._h5.create_dataset('box_vectors', data=box_vectors)
+
+    @property
+    def velocities(self):
+        return self._h5['velocities']
+
+    @velocities.setter
+    def velocities(self, velocities):
+        self._h5.create_dataset('velocities', data=velocities)
+
+    @property
+    def forces(self):
+        return self._h5['forces']
+
+    @forces.setter
+    def forces(self, forces):
+        self._h5.create_dataset('forces', data=forces)
+
+    @property
+    def parameters(self):
+        return self._h5['parameters']
+
+    @parameters.setter
+    def parameters(self, parameters):
+        self._h5.create_dataset('parameters', data=parameters)
+
+    @property
+    def observables(self):
+        return self._h5['observables']
+
+    @observables.setter
+    def observables(self, observables):
+        self._h5.create_dataset('observables', data=observables)
 
 
 
