8e7896982c3023108afe05762c722610c32ca67a
salotz <salotz@salotz.info>
2017-08-25 11:29:43 -0400

    updated setup.py and dependencies in readme
    Should automatically install all dependencies now

--- setup.py
+++ setup.py
@@ -7,8 +7,9 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'openmm',
         'numpy',
-        'mdtraj'
+        'h5py',
+        'openmm',
+        'scoop'
     ],
 )
