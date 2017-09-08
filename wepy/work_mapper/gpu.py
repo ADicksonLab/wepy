ad1316a566b9b65f50d4f136ef235cda02b3c176
salotz <salotz@salotz.info>
2017-09-08 14:24:16 -0400

    added options to either specify gpus or n_workers

--- wepy/work_mapper/gpu.py
+++ wepy/work_mapper/gpu.py
@@ -2,24 +2,38 @@ import multiprocessing as mulproc
 
 class GPUMapper(object):
 
-    def __init__(self, n_walkers, n_workers):
+    def __init__(self, n_walkers, n_workers=None, gpu_indices=None):
 
-        self.n_workers = n_workers
-        self.gpu_indices = range(n_workers)
+        if gpu_indices is not None:
+            self.gpu_indices = gpu_indices
+            self.n_workers = len(gpu_indices)
+        else:
+            assert n_workers, "If gpu_indices are not given the n_workers must be given"
+            self.n_workers = n_workers
+            self.gpu_indices = range(n_workers)
 
-        # TODO add comments describing what is going on
+        # make a Queue for free workers, when one is being used it is
+        # popped off and locked
         self.free_workers = mulproc.Queue()
+        # the semaphore provides the locks on the workers
         self.lock = mulproc.Semaphore(self.n_workers)
+        # initialize a list to put results in
         self.results_list = mulproc.Manager().list()
         for i in range(n_walkers):
             self.results_list.append(None)
+
+        # add the free worker indices (not device/gpu indices) to the
+        # free workers queue
         for i in range(self.n_workers):
             self.free_workers.put(i)
 
     def exec_call(self, _call_, index, *args):
-        # gets a free worker
+
+        # lock a worker, then pop it off the queue so no other process
+        # tries to use it
         self.lock.acquire()
         worker_idx = self.free_workers.get()
+
         # convert this to an available GPU index
         gpu_idx = self.gpu_indices[worker_idx]
 
