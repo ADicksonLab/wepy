841b3308e05c11801b244efa61140834758e6755
Nazanin <nazanin.donyapour@gmail.com>
2017-10-02 17:03:21 -0400

    small changes in gpu examplec

--- examples/Random_Walk/we.py
+++ examples/Random_Walk/we.py
@@ -15,12 +15,14 @@ from wepy.sim_manager import Manager
 
 
 # define a function for saving datat in hdf5 file
-def save_data(file_handler,cycle_idx, resampled_walkers, resampling_records):
+def save_data(file_handler, dimension, cycle_idx, resampled_walkers, resampling_records):
     
      for walker_idx, walker in enumerate(resampled_walkers):
-         file_handler.create_dataset('cycle_{:0>5}/walker_{:0>5}/positions'.format(cycle_idx,walker_idx),
+         file_handler.create_dataset('d_{:0>5}/cycle_{:0>5}/walker_{:0>5}/positions'.
+                                     format(dimension, cycle_idx, walker_idx),
                                      data = walker.positions)
-         file_handler.create_dataset('cycle_{:0>5}/walker_{:0>5}/weight'.format(cycle_idx,walker_idx), data=walker.weight)
+         file_handler.create_dataset('d_{:0>5}/cycle_{:0>5}/walker_{:0>5}/weight'.
+                                     format(dimension, cycle_idx, walker_idx), data=walker.weight)
                      
 
 if __name__ == "__main__":
@@ -28,7 +30,7 @@ if __name__ == "__main__":
     # run this example with -m scoop , without scoop your program does not work    
     #initialize RandomWalkers
     num_walkers = 8
-    dimension = 5
+    dimension = 2
 
     # set up initial state for walkers
     positions = np.zeros((dimension))
@@ -52,6 +54,7 @@ if __name__ == "__main__":
    
     
     # makes ref_traj and selects lingand_atom and protein atom  indices
+
     # instantiate a wexplore2 unbindingboudaryconditiobs
 
     n_cycles = 3
@@ -59,98 +62,81 @@ if __name__ == "__main__":
     debug_prints = True
 
     walkers = init_walkers
-    # the main cycle loop
-    if debug_prints:
-        result_template_str = "|".join(["{:^10}" for i in range(num_walkers + 1)])
-        sys.stdout.write("Starting simulation\n")
+    report_path = 'wepy_results'
+    reporter = WepyHDF5Reporter(report_path, mode='w',
+                                decisions=resampler.DECISION,
+                                instruction_dtypes=resampler.INSTRUCTION_DTYPES,
+                                resampling_aux_dtypes=None,
+                                resampling_aux_shapes=None,
+                                topology='{}')
 
 
-    # create a hdf5 file to write
-    h5file_handler = h5py.File(os.getcwd()+'/wepy_results.h5',mode='w')
-    for cycle_idx in range(n_cycles):
+    # running the simulation
+    sim_manager = Manager(init_walkers,
+                          runner=runner,
+                          resampler=resampler,
+                          work_mapper=scoop.futures.map,
+                          reporter=reporter)
+    n_steps = 10000
+    n_cycles = 100
 
-        if debug_prints:
-            sys.stdout.write("Begin cycle {}\n".format(cycle_idx))
-
-        #run the segment
-        
-        new_walkers = list(map(runner.run_segment,
-                                             walkers,
-                                             (segment_length for i in range(num_walkers)
-                                             )
-                        ))
-
-        if debug_prints:
-            sys.stdout.write("End cycle {}\n".format(cycle_idx))
-
-            # apply rules of boundary conditions and warp walkers through space
-        # warped_walkers, warped_walker_records, warp_aux_data = \
-        #                 self.boundary_conditions.warp_walkers(new_walkers,
-        #                                                       debug_prints=debug_prints)
-
-            # resample walkers
-        resampled_walkers, resampling_records, resampling_aux_data = resampler.resample(new_walkers,
-                                                                                       debug_prints=debug_prints)
-        
-        if debug_prints:
-            # print results for this cycle
-            print("Net state of walkers after resampling:")
-            print("--------------------------------------")
-            # slots
-            slot_str = result_template_str.format("slot",
-                                                  *[i for i in range(len(resampled_walkers))])
-            print(slot_str)
-            # states
-            walker_state_str = result_template_str.format("state",
-                *[str(walker.state) for walker in resampled_walkers])
-            print(walker_state_str)
-            # weights
-            walker_weight_str = result_template_str.format("weight",
-                *[str(walker.weight) for walker in resampled_walkers])
-            print(walker_weight_str)
-            #print warped_walker_records
-
-        # report results to the reporter
-
-        # prepare resampled walkers for running new state changes
-        walkers = resampled_walkers
-
-
+    # run a simulation with the manager for n_steps cycles of length 1000 each
+    steps = [ n_steps for i in range(n_cycles)]
+    print("Running simulation")
+    sim_manager.run_simulation(n_cycles,
+                               steps,
+                               debug_prints=True)
 
 
     
 
+    # # create a hdf5 file to write
+    # h5file_handler = h5py.File(os.getcwd()+'/wepy_results.h5',mode='w')
+    # for dimension in dimensions:
+         
+    #      for cycle_idx in range(n_cycles):
 
+    #          if debug_prints:
+    #              sys.stdout.write("Begin cycle {}\n".format(cycle_idx))
 
+    #          #run the segment
 
-    
-    # # instantiate a reporter for HDF5
-    # report_path = 'wepy_results.h5'
-    # reporter = WepyHDF5Reporter(report_path, mode='w',
-    #                             decisions=resampler.DECISION,
-    #                             instruction_dtypes=resampler.INSTRUCTION_DTYPES,
-    #                             resampling_aux_dtypes=None,
-    #                             resampling_aux_shapes=None,
-    #                              )
-    # # Instantiate a simulation manager
-    # sim_manager = Manager(init_walkers,
-    #                       runner=runner,
-    #                       resampler=resampler,
-    #                       boundary_conditions=None,
-    #                       work_mapper=map,
-    #                       reporter= reporter)
-    # n_steps = 100
-    # n_cycles = 2
+    #          new_walkers = list(map(runner.run_segment,
+    #                                               walkers,
+    #                                               (segment_length for i in range(num_walkers)
+    #                                               )
+    #                          ))
 
-    # # run a simulation with the manager for n_steps cycles of length 1000 each
-    # steps = [ n_steps for i in range(n_cycles)]
-    # print("Running simulation")
-    # sim_manager.run_simulation(n_cycles,
+    #          if debug_prints:
+    #              sys.stdout.write("End cycle {}\n".format(cycle_idx))
+    #              # resample walkers
+    #          resampled_walkers, resampling_records, resampling_aux_data = resampler.resample(new_walkers,
+    #                                                                                         debug_prints=debug_prints)
 
-    #                            steps,
-    #                            debug_prints=True)
+    #          if debug_prints:
+    #              # print results for this cycle
+    #              print("Net state of walkers after resampling:")
+    #              print("--------------------------------------")
+    #              # slots
+    #              slot_str = result_template_str.format("slot",
+    #                                                    *[i for i in range(len(resampled_walkers))])
+    #              print(slot_str)
+    #              # states
+    #              walker_state_str = result_template_str.format("state",
+    #                  *[str(walker.state) for walker in resampled_walkers])
+    #              print(walker_state_str)
+    #              # weights
+    #              walker_weight_str = result_template_str.format("weight",
+    #                  *[str(walker.weight) for walker in resampled_walkers])
+    #              print(walker_weight_str)
+    #              #print warped_walker_records
 
-    # # your data should be in the 'wepy_results.h5'
+    #          # report results to the reporter
+
+    #          # prepare resampled walkers for running new state changes
+    #          walkers = resampled_walkers
+
+             
     
     
     
--- wepy/distance_functions/openmm_distance.py
+++ wepy/distance_functions/openmm_distance.py
@@ -1,6 +1,7 @@
 import numpy as np
-
+import numpy.linalg as la
 import mdtraj as mdj
+
 
 from wepy.openmm import OpenMMRunner, OpenMMWalker
 from wepy.distance_functions.distance import Distance
@@ -11,36 +12,102 @@ class OpenMMDistance(Distance):
         self.ligand_idxs = ligand_idxs
         self.binding_site_idxs = binding_site_idxs
 
-    def rmsd(self, traj, ref, idx):
-        return np.sqrt(3*np.sum(np.square(traj.xyz[:, idx, :] - ref.xyz[:, idx, :]),
-                                axis=(1, 2))/idx.shape[0])
+        
+    def _calc_angle(self, v1, v2):
+        return np.degrees(np.arccos(np.dot(v1, v2)/(la.norm(v1) * la.norm(v2))))
 
-    def maketraj(self, positions):
+    def _calc_length(self, v):
+        return la.norm(v)
+    
+    def _pos_to_array(self, positions):
         n_atoms = self.topology.n_atoms
+
         xyz = np.zeros((1, n_atoms, 3))
 
         for i in range(n_atoms):
-            xyz[0,i,:] = ([positions[i]._value[0], positions[i]._value[1],
-                                                        positions[i]._value[2]])
-        return mdj.Trajectory(xyz, self.topology)
-
+            xyz[0,i,:] = ([positions[i]._value[0],
+                           positions[i]._value[1],
+                           positions[i]._value[2]])
+        return xyz
 
     
-    def calculate_rmsd(self, positions_a, positions_b):
-        traj_a = self.maketraj(positions_a)
-        traj_b = self.maketraj(positions_b)
-        traj_b = traj_b.superpose(traj_a, atom_indices=self.binding_site_idxs)
-        return  self.rmsd(traj_a, traj_b, self.ligand_idxs)
 
+    def rmsd(self, traj, ref, idx):
+        return np.sqrt(np.sum(np.square(traj.xyz[:, idx, :] - ref.xyz[:, idx, :]),
+                                axis=(1, 2))/idx.shape[0])
+    def move_ligand(self, positions, boxsize_x, boxsize_y, boxsize_z):
+        positions = np.copy(positions)
+
+        ligand_center = [np.array((0.0,0.0,0.0))]
+        binding_site_center = [np.array((0.0,0.0,0.0))]
+
+        # calculate center of mass ligand
+        for idx in self.ligand_idxs:
+            ligand_center += positions[:, idx, :]
+
+        ligand_center = ligand_center/len(self.ligand_idxs)
+  
+        for idx in self.binding_site_idxs:
+            binding_site_center += positions[:, idx, :]
+
+        binding_site_center = binding_site_center/len(self.binding_site_idxs)
+        
+        diff = ligand_center - binding_site_center
+
+        V = [np.array((0.0, 0.0, 0.0))]
+         # x direction       
+        if diff[0][0] > boxsize_x /2 :
+            V[0][0] = boxsize_x /2
+        elif  diff[0][0] < -boxsize_x /2 :
+            V[0][0] = -boxsize_x/2
+            #  y direction
+        if diff[0][1] > boxsize_y/2 :
+            V[0][1] = boxsize_y/2
+        elif  diff[0][1] < -boxsize_y/2 :
+            V[0][1] = -boxsize_y/2
+            # z direction
+        if diff[0][2] > boxsize_z /2 :
+            V[0][2] = boxsize_z /2
+        elif  diff[0][1] < -boxsize_z /2 :
+            V[0][2] = -boxsize_z/2
+            # translate  ligand
+        for idx in self.ligand_idxs:
+           positions[:, idx, :] += V
+
+        return positions    
+
+    def maketraj(self, walker):
+        # convert box_vectors to angles and lengths for mdtraj
+        # calc box length
+        cell_lengths = np.array([[self._calc_length(v._value) for v in walker.box_vectors]])
+
+        # TODO order of cell angles
+        # calc angles
+        cell_angles = np.array([[self._calc_angle(walker.box_vectors._value[i],
+                                                 walker.box_vectors._value[j])
+                                 for i, j in [(0,1), (1,2), (2,0)]]])
+
+        # moves ligand inside the box
+        positions = self.move_ligand(self._pos_to_array(walker.positions),
+                                     cell_lengths[0][0], cell_lengths[0][1], cell_lengths[0][2])
+
+        # make a traj out of it so we can calculate distances through
+        # the periodic boundary conditions
+        walker_traj = mdj.Trajectory(positions,
+                                     topology=self.topology,
+                                     unitcell_lengths=cell_lengths,
+                                     unitcell_angles=cell_angles)        
+        return  walker_traj
 
     def distance(self, walkers):
-        n_walkers = len (walkers)
-        distance_matrix = np.zeros((n_walkers, n_walkers))
-        for i in range(n_walkers):
-            for j in range(i+1, n_walkers):
-                d = self.calculate_rmsd(walkers[i].positions[0:self.topology.n_atoms],
-                                        walkers[j].positions[0:self.topology.n_atoms])
-                                
+        num_walkers = len(walkers)
+        distance_matrix = np.zeros((num_walkers, num_walkers))
+        for i in range(num_walkers):
+            ref_traj = self.maketraj(walkers[i])
+            for j in range(i+1, num_walkers):
+                target_traj = self.maketraj(walkers[j])
+                target_traj = target_traj.superpose(ref_traj, atom_indices=self.binding_site_idxs)
+                d = self.rmsd(target_traj, ref_traj, self.ligand_idxs)              
                 distance_matrix[i][j] = d
                 distance_matrix [j][i] = d
 
