a604e15862477a84b8e5d47dde3204aa50953d88
salotz <salotz@salotz.info>
2017-10-10 20:29:59 -0400

    added new iter_trajs_fields method for new compute_observables
    couldn't serialize h5py references so message passing them for
    parallel stuff wouldn't work. So I made a new iter function that you
    tell it which fields to pass and sends numpy arrays of them back in
    the iteration.
    
    This is used in the compute observables so we can do things in parallel

--- wepy/hdf5.py
+++ wepy/hdf5.py
@@ -1660,6 +1660,33 @@ class WepyHDF5(object):
             else:
                 yield traj
 
+    def iter_trajs_fields(self, fields, idxs=False, traj_sel=None):
+        """Generator for all of the specified non-compound fields
+        h5py.Datasets for all trajectories in the dataset across all
+        runs. Fields is a list of valid relative paths to datasets in
+        the trajectory groups.
+
+        """
+
+        for idx_tup, traj in self.iter_trajs(idxs=True, traj_sel=traj_sel):
+            run_idx, traj_idx = idx_tup
+
+            dsets = {}
+            for field in fields:
+                try:
+                    dset = traj[field][:]
+                except KeyError:
+                    warn("field \"{}\" not found in \"{}\"".format(field, traj.name), RuntimeWarning)
+                    dset = None
+
+                dsets[field] = dset
+
+            if idxs:
+                yield (run_idx, traj_idx), dsets
+            else:
+                yield dsets
+
+    # TODO DEPRECATE replaced by `iter_trajs_fields`
     def iter_trajs_field(self, field, idxs=False, traj_sel=None):
         """Generator for all of the specified non-compound fields
         h5py.Datasets for all trajectories in the dataset across all
@@ -1683,6 +1710,7 @@ class WepyHDF5(object):
                 yield dset
 
 
+    # TODO DEPRECATE replaced by `iter_trajs_fields`
     def iter_trajs_compound_field(self, grp, field, idxs=False, traj_sel=None):
         """Generator for all of the specified non-compound fields
         h5py.Datasets for all trajectories in the dataset across all
@@ -1777,8 +1805,6 @@ class WepyHDF5(object):
                  error. Otherwise single values will be automatically
                  mapped to all trajectories.
 
-        **kwargs : same as *args, but will pass all kwargs to the func.
-
         """
 
         # check the args and kwargs to see if they need expanded for
@@ -1804,7 +1830,64 @@ class WepyHDF5(object):
         else:
             return results
 
-    def compute_observable(self, func, *args,
+    def traj_fields_map(self, func, fields, *args, map_func=map, idxs=False, traj_sel=None):
+        """Function for mapping work onto field of trajectories in the
+        WepyHDF5 file object. Similar to traj_map, except `h5py.Group`
+        objects cannot be pickled for message passing. So we select
+        the fields to serialize instead and pass the `numpy.ndarray`s
+        to have the work mapped to them.
+
+        func : the function that will be mapped to trajectory groups
+
+        fields : list of fields that will be serialized into a dictionary
+                 and passed to the map function. These must be valid
+                 `h5py` path strings relative to the trajectory
+                 group. These include the standard fields like
+                 'positions' and 'weights', as well as compound paths
+                 e.g. 'observables/sasa'.
+
+        map_func : the function that maps the function. This is where
+                        parallelization occurs if desired.  Defaults to
+                        the serial python map function.
+
+        traj_sel : a trajectory selection. This is a valid `traj_sel`
+        argument for the `iter_trajs` function.
+
+        *args : additional arguments to the function. If this is an
+                 iterable it will be assumed that it is the appropriate
+                 length for the number of trajectories, WARNING: this will
+                 not be checked and could result in a run time
+                 error. Otherwise single values will be automatically
+                 mapped to all trajectories.
+
+        """
+
+        # check the args and kwargs to see if they need expanded for
+        # mapping inputs
+        mapped_args = []
+        for arg in args:
+            # if it is a sequence or generator we keep just pass it to the mapper
+            if isinstance(arg, Sequence) and not isinstance(arg, str):
+                assert len(arg) == self.n_trajs, "Sequence has fewer"
+                mapped_args.append(arg)
+            # if it is not a sequence or generator we make a generator out
+            # of it to map as inputs
+            else:
+                mapped_arg = (arg for i in range(self.n_trajs))
+                mapped_args.append(mapped_arg)
+
+        results = map_func(func, self.iter_trajs_fields(fields, traj_sel=traj_sel, idxs=False),
+                           *mapped_args)
+
+        if idxs:
+            if traj_sel is None:
+                traj_sel = self.run_traj_idx_tuples()
+            return zip(traj_sel, results)
+        else:
+            return results
+
+
+    def compute_observable(self, func, fields, *args,
                            map_func=map, traj_sel=None, save_to_hdf5=None, idxs=False):
         """Compute an observable on the trajectory data according to a
         function. Optionally save that data in the observables data group for
@@ -1821,7 +1904,8 @@ class WepyHDF5(object):
             # DEBUG enforce this until sparse trajectories are implemented
             # assert traj_sel is None, "no selections until sparse trajectory data is implemented"
 
-        results = self.traj_map(func, *args, map_func=map_func, traj_sel=traj_sel, idxs=True)
+        results = self.traj_fields_map(func, fields, *args,
+                                       map_func=map_func, traj_sel=traj_sel, idxs=True)
 
         # if we are saving this to the trajectories observables add it as a dataset
         if save_to_hdf5:
