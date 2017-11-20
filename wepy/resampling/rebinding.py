fixed factor of 10 in spread function

wepy/resampling/rebinding.py
@@ -18,7 +18,12 @@ from wepy.resampling.clone_merge import NothingInstructionRecord, CloneInstructi
                                         SquashInstructionRecord, KeepMergeInstructionRecord
 from wepy.resampling.clone_merge import CloneMergeDecision, CLONE_MERGE_INSTRUCTION_DTYPES
 
-class OpenMMDistance(object):
+class OpenMMUnbindingDistance(object):
+    # The distance function here returns a distance matrix where the element (d_ij) is the
+    # RMSD between walkers i and j.  The RMSD is computed using the geomm package, by aligning
+    # to the binding site atoms, and taking the RMSD of the ligand atoms.  It uses alternative maps
+    # for the binding site atoms when defined, and aligns to all alternative maps, returning the
+    # minimum RMSD computed over all maps.
     def __init__(self, topology= None, ligand_idxs=None, binding_site_idxs=None, alt_maps=None):
         self.topology = topology
         self.ligand_idxs = ligand_idxs
@@ -87,13 +92,110 @@ class OpenMMDistance(object):
         
         return d
 
+class OpenMMRebindingDistance(object):
+    # The distance function here returns a distance matrix where the element (d_ij) is the
+    # difference between 1/RMSD_0(i) and 1/RMSD_0(j).  Where RMSD_0(i) is the RMSD of walker i
+    # to the reference structure (comp_xyz), which is typically the crystallographic bound state.
+    # The RMSDs to the bound state are computed using the geomm package, by aligning
+    # to the binding site atoms, and taking the RMSD of the ligand atoms.  It uses alternative maps
+    # for the binding site atoms when defined, and aligns to all alternative maps, returning the
+    # minimum RMSD computed over all maps.
+    def __init__(self, topology= None, ligand_idxs=None, binding_site_idxs=None, alt_maps=None, comp_xyz=None):
+        self.topology = topology
+        self.ligand_idxs = ligand_idxs
+        self.binding_site_idxs = binding_site_idxs
+        self.alt_maps = alt_maps
+        
+        self.comp_traj = self._make_comp_traj(comp_xyz)
+        # alt_maps are alternative mappings to the binding site
+        # this program now assumes that all atoms in alternative maps are contained in binding_site_idxs list
+        # comp_xyz are the xyz coordinates of a reference state (usually the bound state)
+        # this assumes that the xyz comes from the same topology
+
+    def _make_comp_traj(self, comp_xyz):
+        small_lig_idxs = np.array(range(len(self.ligand_idxs)))
+        small_bs_idxs = np.array(range(len(self.ligand_idxs),len(self.ligand_idxs)+len(self.binding_site_idxs)))
+        keep_atoms = np.concatenate((self.ligand_idxs,self.binding_site_idxs),axis=0)
+        small_top = self.topology.subset(keep_atoms)
+#        import pdb; pdb.set_trace()
+        small_pos = np.array(comp_xyz)[:,keep_atoms,:]
+        
+        return mdj.Trajectory(small_pos,small_top)
+        
+    def _calc_angle(self, v1, v2):
+        return np.degrees(np.arccos(np.dot(v1, v2)/(la.norm(v1) * la.norm(v2))))
+
+    def _calc_length(self, v):
+        return la.norm(v)
+
+    def _xyz_from_walkers(self, walkers, keep_atoms=[]):
+        if len(keep_atoms) == 0:
+            keep_atoms = range(np.shape(walkers[0].positions)[0])
+            
+        return np.stack(([np.array(w.positions.value_in_unit(unit.nanometer))[keep_atoms,:] for w in walkers]),axis=0)
+
+    def _box_from_walkers(self, walkers):
+        return np.stack(([np.array([la.norm(v._value) for v in w.box_vectors]) for w in walkers]),axis=0)
+        
+    def get_rmsd_native(self, walkers):
+        num_walkers = len(walkers)
+        distance_matrix = np.zeros((num_walkers, num_walkers))
+
+        small_lig_idxs = np.array(range(len(self.ligand_idxs)))
+        small_bs_idxs = np.array(range(len(self.ligand_idxs),len(self.ligand_idxs)+len(self.binding_site_idxs)))
+        keep_atoms = np.concatenate((self.ligand_idxs,self.binding_site_idxs),axis=0)
+        
+        small_pos = self._xyz_from_walkers(walkers,keep_atoms)
+        box_lengths = self._box_from_walkers(walkers)
+        newpos_small = geomm.recentering.recenter_receptor_ligand(small_pos,box_lengths,ligand_idxs=small_lig_idxs,receptor_idxs=small_bs_idxs)
+
+        small_top = self.topology.subset(keep_atoms)
+        traj_rec = mdj.Trajectory(newpos_small,small_top)
+
+        traj_rec.superpose(self.comp_traj,atom_indices=small_bs_idxs)
+        rmsd_native = np.zeros((num_walkers))
+        for i in range(num_walkers):
+            rmsd_native[i] = geomm.rmsd.rmsd_one_frame(traj_rec.xyz[i],self.comp_traj.xyz[0],small_lig_idxs)
+
+        if self.alt_maps is not None:
+            # figure out the "small" alternative maps
+            small_alt_maps = deepcopy(self.alt_maps)
+            for i, a in enumerate(small_alt_maps):
+                for j, e in enumerate(a):
+                    try:
+                        small_alt_maps[i][j] = self.binding_site_idxs.index(e)
+                    except:
+                        raise Exception('Alternative maps are assumed to be permutations of existing binding site indices')
+
+            for alt_map in small_alt_maps:
+                alt_traj_rec = mdj.Trajectory(newpos_small,small_top)
+                alt_traj_rec.superpose(self.comp_traj,atom_indices=small_bs_idxs,ref_atom_indices=alt_map)
+                for i in range(num_walkers):
+                    dtest = geomm.rmsd.rmsd_one_frame(alt_traj_rec.xyz[i],self.comp_traj.xyz[0],small_lig_idxs)
+                    if dtest < rmsd_native[i]:
+                        rmsd_native[i] = dtest
+        return rmsd_native
+        
+    def distance(self, walkers):
+        num_walkers = len(walkers)
+        rmsd_native = self.get_rmsd_native(walkers)
+        d = np.zeros((num_walkers,num_walkers))
+        for i in range(num_walkers-1):
+            d[i][i] = 0
+            for j in range(i+1,num_walkers):
+                d[i][j] = abs(1./rmsd_native[i] - 1./rmsd_native[j])                
+                d[j][i] = d[i][j]
+
+        return d
+
+    
 class WExplore2Resampler(Resampler):
 
     DECISION = CloneMergeDecision
     INSTRUCTION_DTYPES = CLONE_MERGE_INSTRUCTION_DTYPES
 
     def __init__(self, seed=None, pmin=1e-12, pmax=0.1, dpower=4, merge_dist=0.25,
-                 topology=None, ligand_idxs=None, binding_site_idxs=None, alternative_maps=None):
+                 topology=None, ligand_idxs=None, binding_site_idxs=None, alternative_maps=None, distance_function='unbinding', comp_xyz=None):
         self.pmin=pmin
         self.lpmin = np.log(pmin/100)
         self.pmax=pmax
@@ -105,8 +207,14 @@ class WExplore2Resampler(Resampler):
         self.topology = topology
         self.ligand_idxs = ligand_idxs
         self.binding_site_idxs = binding_site_idxs
-        self.distance_function = OpenMMDistance(self.topology,
-                                                self.ligand_idxs, self.binding_site_idxs, alternative_maps)
+        if distance_function == 'unbinding':
+            self.distance_function = OpenMMUnbindingDistance(self.topology,
+                                                             self.ligand_idxs, self.binding_site_idxs, alternative_maps)
+        elif distance_function == 'rebinding':
+            self.distance_function = OpenMMRebindingDistance(self.topology,
+                                                             self.ligand_idxs, self.binding_site_idxs, alternative_maps, comp_xyz)
+        else:
+            raise Exception('distance function',distance_function,'is not recognized!')
 
     def _calcspread(self, n_walkers, walkerwt, amp, distance_matrix):
         spread = 0