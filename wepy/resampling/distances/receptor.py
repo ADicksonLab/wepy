e3bc0f7242ba2347050561360382172bc0f3d15c
salotz <salotz@salotz.info>
2018-03-06 14:38:59 -0500

    fix to unbinding distance
    needed to take into account the internal idxs of the ligand and
    binding site in the image

--- wepy/resampling/distances/receptor.py
+++ wepy/resampling/distances/receptor.py
@@ -8,13 +8,24 @@ from wepy.resampling.distances.distance import Distance
 class UnbindingDistance(Distance):
     def __init__(self, ligand_idxs, binding_site_idxs):
 
+        # the idxs of the ligand and binding site from the whole state
         self._lig_idxs = ligand_idxs
         self._bs_idxs = binding_site_idxs
 
+        # number of atoms in each
+        self._n_lig_atoms = len(self._lig_idxs)
+        self._n_bs_atoms = len(self._bs_idxs)
+
+        # the idxs used for the whole image
+        self._image_idxs = np.concatenate( (self._lig_idxs, self._bs_idxs) )
+
+        # the idxs of the ligand and binding site within the image
+        self._image_lig_idxs = np.arange(self._n_lig_atoms)
+        self._image_bs_idxs = np.arange(self._n_lig_atoms, self._n_lig_atoms + self._n_bs_atoms)
+
     def image(self, state):
 
-        keep_idxs = np.concatenate( (self._lig_idxs, self._bs_idxs) )
-        state_image = state[keep_idxs]
+        state_image = state['positions'][self._image_idxs]
 
         return state_image
 
@@ -23,11 +34,11 @@ class UnbindingDistance(Distance):
         # first we superimpose the two structures according to the
         # binding site, and by translating and rotating only one of
         # the images, image_b by convention.
-        sup_image_b = superimpose(image_a, image_b, idxs=self._bs_idxs)
+        sup_image_b = superimpose(image_a, image_b, idxs=self._image_bs_idxs)
 
 
         # then we calculate the rmsd of only the ligands between the
         # images
-        lig_rmsd = calc_rmsd(image_a, sup_image_b, idxs=self._lig_idxs)
+        lig_rmsd = calc_rmsd(image_a, sup_image_b, idxs=self._image_lig_idxs)
 
         return lig_rmsd
