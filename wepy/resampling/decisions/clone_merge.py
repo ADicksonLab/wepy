7c4e2c9947488b1d1b91b6a4d6fb9351154fc3ca
Samuel Lotz <samuel.lotz@salotz.info>
2018-08-13 11:38:16 -0400

    changed index for keep merges
    this wouldn't be a problem for current algorithms but in general you
    should use teh keep_idx which is not necessarily the same as the
    original KEEP_MERGE walker idx

--- wepy/resampling/decisions/clone_merge.py
+++ wepy/resampling/decisions/clone_merge.py
@@ -139,13 +139,8 @@ class MultiCloneMergeDecision(Decision):
             # do the merging for each merge group
             for target_idx, walker_idxs in squash_walkers.items():
 
-                # DEBUG
-                try:
-                    keep_idx = keep_walkers[target_idx]
-                except:
-                    import ipdb; ipdb.set_trace()
-
                 keep_idx = keep_walkers[target_idx]
+
                 # collect the walkers in the merge group, the keep idx is
                 # always the first in the list
                 merge_grp = [walkers[keep_idx]] + [walkers[i] for i in walker_idxs]
@@ -159,7 +154,7 @@ class MultiCloneMergeDecision(Decision):
                         "Multiple walkers assigned to position {}".format(target_idx))
 
                 # set it in the slot for the keep_idx
-                mod_walkers[target_idx] = merged_walker
+                mod_walkers[keep_idx] = merged_walker
 
 
         if not all([False if walker is None else True for walker in mod_walkers]):
@@ -167,102 +162,3 @@ class MultiCloneMergeDecision(Decision):
             raise ValueError("Some walkers were not created")
 
         return mod_walkers
-
-
-
-
-
-
-# class CloneMergeDecision(Decision):
-
-#     ENUM = CloneMergeDecisionEnum
-
-#     # namedtuple records for each decision type
-#     INSTRUCTION_RECORDS = (
-#         (ENUM.NOTHING, namedtuple("NothingInstructionRecord", ['slot'])),
-#         (ENUM.CLONE, namedtuple("CloneInstructionRecord", ['slot_a', 'slot_b'])),
-#         (ENUM.SQUASH, namedtuple("SquashInstructionRecord", ['merge_slot'])),
-#         (ENUM.KEEP_MERGE, namedtuple("KeepMergeInstructionRecord", ['slot'])),
-#     )
-
-#     # datatypes for each instruction, useful for allocating memory in
-#     # databases. Datatypes may be converted to a numpy dtype by
-#     # calling `dtype(instruct_dtype)`
-#     INSTRUCTION_DTYPES = (
-#         (ENUM.NOTHING, [('pos', np.int)]),
-#         (ENUM.CLONE, [('slot_a', np.int), ('slot_b', np.int)]),
-#         (ENUM.SQUASH, [('merge_to', np.int)]),
-#         (ENUM.KEEP_MERGE, [('pos', np.int)]),
-#     )
-
-#     @classmethod
-#     def action(cls, walkers, decisions):
-#         """Performs cloning and merging according to a list of resampling
-#         records for some walkers."""
-
-#         # list for the modified walkers
-#         mod_walkers = [None for i in range(len(walkers))]
-
-#         # we need to collect groups of merges, one entry for each
-#         # merge, where the key is the walker_idx of the keep merge slot
-#         squash_walkers = defaultdict(list)
-#         keep_walkers = {}
-#         # go through each decision and perform the decision
-#         # instructions
-#         for walker_idx, decision in enumerate(decisions):
-#             decision_value, instruction = decision
-
-#             if decision_value == cls.ENUM.NOTHING.value:
-#                 # check to make sure a walker doesn't already exist
-#                 # where you are going to put it
-#                 if mod_walkers[instruction[0]] is not None:
-#                     raise ValueError(
-#                         "Multiple walkers assigned to position {}".format(instruction[0]))
-
-#                 # put the walker in the position specified by the
-#                 # instruction
-#                 mod_walkers[instruction[0]] = walkers[walker_idx]
-
-#             elif decision_value == cls.ENUM.CLONE.value:
-#                 walker = walkers[walker_idx]
-#                 clones = split(walker, number=len(instruction))
-
-#                 for i, walker_idx in enumerate(instruction):
-#                     if mod_walkers[walker_idx] is not None:
-#                         raise ValueError(
-#                             "Multiple walkers assigned to position {}".format(instruction[0]))
-#                     mod_walkers[walker_idx] = clones[i]
-
-#             # if it is a decision for merging we must perform this
-#             # once we know all the merge targets for each merge group
-#             elif decision_value == cls.ENUM.SQUASH.value:
-
-#                 # save this walker to the appropriate merge group to
-#                 # merge after going through the list of walkers
-#                 squash_walkers[instruction[0]].append(walker_idx)
-
-#             elif decision_value == cls.ENUM.KEEP_MERGE.value:
-#                 keep_walkers[instruction[0]] = walker_idx
-
-#             else:
-#                 raise ValueError("Decision not recognized")
-
-#         # do the merging for each merge group
-#         for target_idx, walker_idxs in squash_walkers.items():
-#             keep_idx = keep_walkers[target_idx]
-#             # collect the walkers in the merge group, the keep idx is
-#             # always the first in the list
-#             merge_grp = [walkers[keep_idx]] + [walkers[i] for i in walker_idxs]
-
-#             # merge the walkers
-#             merged_walker = keep_merge(merge_grp, 0)
-
-#             # make sure there is not already a walker in this slot
-#             if mod_walkers[target_idx] is not None:
-#                 raise ValueError(
-#                     "Multiple walkers assigned to position {}".format(target_idx))
-
-#             # set it in the slot for the keep_idx
-#             mod_walkers[target_idx] = merged_walker
-
-#         return mod_walkers
