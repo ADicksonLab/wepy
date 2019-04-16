7f702c9c02f6a92e8e1effee9b0f30fabde51865
Samuel Lotz <samuel.lotz@salotz.info>
2019-04-16 11:23:42 -0400

    modifies kv store to just store bytes
    [body]
    
    The module I copied did things via JSON, which really isn't what I
    need since I will be storing a lot of pickles, so I just simplified it
    and made it do no preprocessing with JSON and just stores whatever you
    give it as long as it is bytes or bytearray.
    
    Nothing could possibly depend on this so I am eliding the breakage keywords.
    
    changes = [:changes/fixation]

--- wepy/util/kv.py
+++ wepy/util/kv.py
@@ -32,21 +32,29 @@ import sqlite3
 import sys
 from collections import MutableMapping
 from contextlib import contextmanager
-try:
-    import simplejson as json
-except ImportError:
-    import json  # noqa
-
 
 class KV(MutableMapping):
 
     def __init__(self, db_uri=':memory:', table='data', timeout=5):
-        self._db = sqlite3.connect(db_uri, timeout=timeout)
+        self._db_uri = db_uri
+        self._db = sqlite3.connect(self._db_uri, timeout=timeout)
         self._db.isolation_level = None
         self._table = table
         self._execute('CREATE TABLE IF NOT EXISTS %s '
                       '(key PRIMARY KEY, value)' % self._table)
         self._locks = 0
+
+    @property
+    def table(self):
+        return self._table
+
+    @property
+    def db_uri(self):
+        return self._db_uri
+
+    @property
+    def db(self):
+        return self._db
 
     def _execute(self, *args):
         return self._db.cursor().execute(*args)
@@ -56,28 +64,40 @@ class KV(MutableMapping):
         return n
 
     def __getitem__(self, key):
+
         if key is None:
-            q = ('SELECT value FROM %s WHERE key is NULL' % self._table, ())
+            query = ('SELECT value FROM %s WHERE key is NULL' % self._table, ())
         else:
-            q = ('SELECT value FROM %s WHERE key=?' % self._table, (key,))
-        for row in self._execute(*q):
-            return json.loads(row[0])
-        else:
+            query = ('SELECT value FROM %s WHERE key=?' % self._table, (key,))
+
+        cursor = self._execute(*query)
+        result = cursor.fetchone()
+
+        if result is None:
             raise KeyError
+        else:
+            return result[0]
 
     def __iter__(self):
         return (key for [key] in self._execute('SELECT key FROM %s' %
                                                self._table))
 
     def __setitem__(self, key, value):
-        jvalue = json.dumps(value)
+        """Set a value, must be in bytes format."""
+
+        assert isinstance(value, (bytes, bytearray)), "Value must be bytes or bytearray"
+
         with self.lock():
+
+            # insert the key-value pair if the key isn't in the db
             try:
                 self._execute('INSERT INTO %s VALUES (?, ?)' % self._table,
-                              (key, jvalue))
+                              (key, value))
+
+            # otherwise update the keys value
             except sqlite3.IntegrityError:
                 self._execute('UPDATE %s SET value=? WHERE key=?' %
-                              self._table, (jvalue, key))
+                              self._table, (value, key))
 
     def __delitem__(self, key):
         if key in self:
