f3b2103328af19d6f1fadd0ba31590557d53c4fe
Samuel Lotz <samuel.lotz@salotz.info>
2019-11-03 20:08:05 -0500

    adding gh-pages branch for website

--- sphinx/deploy.sh
+++ sphinx/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-rsync -rav ./_build/html/ salotz@volta.bch.msu.edu:/volume1/web/wepy/
+# we build directly into this repository
+# rsync -rav ./_build/html/ salotz@volta.bch.msu.edu:/volume1/web/wepy/
