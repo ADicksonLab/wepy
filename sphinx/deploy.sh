453000a80de07f1ee80b8f5f8b9269289596ea0f
Samuel Lotz <samuel.lotz@salotz.info>
2019-11-03 22:28:04 -0500

    stupid thing

--- sphinx/deploy.sh
+++ sphinx/deploy.sh
@@ -2,8 +2,6 @@
 
 # we build directly into this repository
 # rsync -rav ./_build/html/ salotz@volta.bch.msu.edu:/volume1/web/wepy/
-<<<<<<< HEAD
-=======
 
 # make sure we have the remote and have fetched the branch
 git remote add github git@github.com:ADicksonLab/wepy.git
@@ -15,4 +13,4 @@ git checkout gh-pages
 cp ./_build/html/* ../docs/
 
 git checkout master
->>>>>>> master
+
