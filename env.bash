36733b9c7b4d949c34a71a857b840529f542667f
Samuel Lotz <samuel.lotz@salotz.info>
2020-04-17 10:58:12 -0400

    cleaning up and moving things adround for new tooling

--- env.bash
+++ env.bash
@@ -1,6 +1,2 @@
-# figure out which directory is you anaconda dir
-export ANACONDA_DIR=$(dirname $(dirname $(which conda)))
-
-
 # enable tab completion for invoke
 source <(inv --print-completion-script bash)
