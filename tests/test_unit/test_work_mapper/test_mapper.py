90eeba41db6cf55fc2583b213299453df0998898
Samuel Lotz <samuel.lotz@salotz.info>
2020-04-17 11:34:34 -0400

    migrated tests from wepy-tests
    we don't have assets and data anymore in there since it is installed
    with openmmtools
    
    We should take a similar route for other testings and just have it as
    a dependency

---
+++ tests/test_unit/test_work_mapper/test_mapper.py
@@ -0,0 +1,129 @@
+import multiprocessing as mp
+import logging
+from copy import deepcopy
+import time
+
+import pytest
+
+from wepy.walker import Walker, WalkerState
+from wepy.work_mapper.mapper import Mapper, TaskException
+from wepy.work_mapper.worker import Worker, WorkerMapper, WorkerException
+from wepy.work_mapper.task_mapper import TaskMapper, WalkerTaskProcess, TaskProcessException
+
+ARGS = (0,1,2)
+
+def gen_walkers():
+
+    return [Walker(WalkerState(**{'num' : arg}), 1/len(ARGS))
+            for arg in ARGS]
+
+
+# test basic functionality
+def task_pass(walker):
+
+    # simulate it actually taking some time
+    n = walker.state['num']
+    return Walker(WalkerState(**{'num' : n+1}), walker.weight)
+
+
+TASK_PASS_ANSWER = [n + 1 for n in ARGS]
+
+class TestWorkMappers():
+
+
+    def test_mapper(self):
+
+        mapper = Mapper(segment_func=task_pass)
+
+        mapper.init()
+
+        results = mapper.map(gen_walkers())
+
+        assert all([res.state['num'] == TASK_PASS_ANSWER[i]
+                    for i, res in enumerate(results)])
+
+        mapper.cleanup()
+
+    def test_worker_mapper(self):
+
+        mapper = WorkerMapper(segment_func=task_pass,
+                              num_workers=3,
+                              worker_type=Worker)
+
+        mapper.init()
+
+        results = mapper.map(gen_walkers())
+
+        assert all([res.state['num'] == TASK_PASS_ANSWER[i]
+                    for i, res in enumerate(results)])
+
+        mapper.cleanup()
+
+    def test_task_mapper(self):
+
+        mapper = TaskMapper(segment_func=task_pass,
+                            num_workers=3,
+                            walker_task_type=WalkerTaskProcess)
+
+        mapper.init()
+
+        results = mapper.map(gen_walkers())
+
+        assert all([res.state['num'] == TASK_PASS_ANSWER[i]
+                    for i, res in enumerate(results)])
+
+        mapper.cleanup()
+
+        time.sleep(1)
+
+# test that task failures are passed up properly
+def task_fail(walker):
+
+    n = walker.state['num']
+    if n == 1:
+        raise ValueError("No soup for you!!")
+    else:
+        return Walker(WalkerState(**{'num' : n+1}), walker.weight)
+
+
+class TestTaskFail():
+
+    ARGS = ((0,1,2),)
+
+    def test_mapper(self):
+
+        mapper = Mapper(segment_func=task_fail)
+
+        mapper.init()
+
+        with pytest.raises(TaskException) as task_exc_info:
+            results = mapper.map(gen_walkers())
+
+        mapper.cleanup()
+
+    def test_worker_mapper(self):
+
+        mapper = WorkerMapper(segment_func=task_fail,
+                              num_workers=3,
+                              worker_type=Worker)
+
+        mapper.init()
+
+        with pytest.raises(TaskException) as task_exc_info:
+            results = mapper.map(gen_walkers())
+
+        mapper.cleanup()
+
+
+    def test_task_mapper(self):
+
+        mapper = TaskMapper(segment_func=task_fail,
+                            num_workers=3,
+                            walker_task_type=WalkerTaskProcess)
+
+        mapper.init()
+
+        with pytest.raises(TaskException) as task_exc_info:
+            results = mapper.map(gen_walkers())
+
+        mapper.cleanup()
