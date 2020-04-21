913651832e081d1e16c3d099500575c31293ae21
Samuel Lotz <samuel.lotz@salotz.info>
2020-04-20 20:35:16 -0400

    wrote tests for Lennard_Jones_Pair example

--- tests/test_docs/test_examples/test_Lennard_Jones_Pair.py
+++ tests/test_docs/test_examples/test_Lennard_Jones_Pair.py
@@ -2,19 +2,12 @@ import os
 import os.path as osp
 from pathlib import Path
 
-import delegator
+from pytest_shutil.cmdline import chdir
+from pytest_shutil.run import run, run_as_main
 
-# the helper modules for testing
-from myutils import (
-    cd,
-)
+### Tests
 
-
-## write one test per example
-
-# Lennard_Jones_Pair
-
-def test_lennard_jones_pair(datadir_factory, printer):
+def test_dir(datadir_factory, printer):
 
     example = "Lennard_Jones_Pair"
 
@@ -25,19 +18,106 @@ def test_lennard_jones_pair(datadir_factory, printer):
     assert (datadir / "source").is_dir()
 
 
-    ## trivial_run.py
+def test_trivial_run(datadir_factory, printer):
 
-    assert (datadir / "source/trivial_run.py").is_file()
+    example = "Lennard_Jones_Pair"
 
-    with cd(datadir):
-        assert Path(os.getcwd()) == datadir
-        proc = delegator.run([
+    datadir = datadir_factory.mkdatadir(f'../_examples/{example}')
+
+    with chdir(datadir):
+        run(['python',
+            'source/trivial_run.py',
+            ],
+        )
+
+def test_sim_maker_run(datadir_factory, printer):
+
+    example = "Lennard_Jones_Pair"
+
+    datadir = datadir_factory.mkdatadir(f'../_examples/{example}')
+
+    with chdir(datadir):
+        run(['python',
+            'source/sim_maker_run.py',
+            '2', '100', '10', '1', 'Reference', 'NoResampler'],
+        )
+
+        run(['python',
+            'source/sim_maker_run.py',
+            '2', '100', '10', '1', 'Reference', 'WExploreResampler'],
+        )
+        run(['python',
+            'source/sim_maker_run.py',
+            '2', '100', '10', '1', 'Reference', 'REVOResampler'],
+        )
+        run(['python',
+            'source/sim_maker_run.py',
+            '2', '100', '10', '1', 'CPU', 'NoResampler'],
+        )
+        run(['python',
+            'source/sim_maker_run.py',
+            '2', '100', '10', '1', 'CPU', 'WExploreResampler'],
+        )
+        run(['python',
+            'source/sim_maker_run.py',
+            '2', '100', '10', '1', 'CPU', 'REVOResampler'],
+        )
+
+def test_we_analysis(datadir_factory, printer):
+
+    example = "Lennard_Jones_Pair"
+
+    datadir = datadir_factory.mkdatadir(f'../_examples/{example}')
+
+    printer("Testing from inside test_we_analysis")
+
+    with chdir(datadir):
+
+        printer(f"Datadir: {datadir}")
+        printer(f"Current dir: {os.getcwd()}")
+
+        out = run([
             "python",
-            "source/trivial_run.py",
+            "source/we.py",
+            "10", '100', "10"])
+
+        printer(out)
+
+        out = run([
+            "python",
+            "source/compute_distance_observable.py",
         ])
 
-    if not proc.ok:
-        printer("Error for trivial_run.py:")
-        printer(proc.err)
+        printer(out)
 
-    assert proc.ok
+        out = run([
+            "python",
+            "source/state_network.py",
+        ])
+
+        printer(out)
+
+
+        ### Tangled sources
+
+        out = run([
+            "python",
+            "_tangle_source/inspect_observable.py",
+        ])
+
+        printer(out)
+
+        out = run([
+            "bash",
+            "./_tangle_source/run0.bash",
+        ]
+        )
+
+        printer(out)
+
+        out = run([
+            "bash",
+            "./_tangle_source/run1.bash",
+        ]
+        )
+
