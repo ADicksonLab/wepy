131ba598851732d3d3a0efa89ca72163b4310337
Samuel Lotz <samuel.lotz@salotz.info>
2020-05-27 16:37:13 -0400

    old smatterings of stuff, will update for prometheus oriented one soon

---
+++ jigs/eliot/source/prometheus.py
@@ -0,0 +1,106 @@
+"""Very basic example of a simulation without a resampler or boundary
+conditions"""
+
+from copy import copy
+import sys
+import logging
+
+from prometheus_client import (
+    write_to_textfile,
+    CollectorRegistry,
+    Summary,
+    ProcessCollector,
+)
+
+from eliot import start_action, to_file
+from eliot.stdlib import EliotHandler
+
+## Logging
+
+
+# logger = logging.getLogger()
+# logger.setLevel(logging.DEBUG)
+
+# logger.addHandler(EliotHandler)
+
+
+# install_mp_handler()
+
+# to_file(sys.stdout)
+
+## Monitoring
+
+# process = ProcessCollector()
+
+### Setup Junk
+with start_action(action_type="Setup") as setup_cx:
+
+    ## Application Imports
+    with start_action(action_type="imports"):
+
+        import simtk.openmm as omm
+        import simtk.unit as unit
+
+        from openmm_systems.test_systems import LennardJonesPair
+
+        from wepy.resampling.resamplers.resampler import NoResampler
+        from wepy.runners.openmm import OpenMMRunner, gen_walker_state
+        from wepy.walker import Walker
+        from wepy.sim_manager import Manager
+
+        from wepy.work_mapper.mapper import Mapper
+        # from wepy.work_mapper.thread import ThreadMapper
+
+
+    # use a ready made system for OpenMM MD simulation
+    with start_action(action_type="Instantiate test system"):
+        test_sys = LennardJonesPair()
+
+    with start_action(action_type="Gen Runner"):
+        integrator = omm.LangevinIntegrator(300.0*unit.kelvin,
+                                            1/unit.picosecond,
+                                            0.002*unit.picoseconds)
+
+        init_state = gen_walker_state(test_sys.positions, test_sys.system, integrator)
+
+        runner = OpenMMRunner(test_sys.system, test_sys.topology, integrator,
+                              platform='Reference')
+
+    # a trivial resampler which does nothing
+    with start_action(action_type="Instantiate Resampler"):
+        resampler = NoResampler()
+
+    # Run the simulation
+
+    # number of cycles of WE to perform
+    n_cycles = 1
+
+    # the number of MD dynamics steps for each cycle
+    n_steps = 1000000
+    steps = [n_steps for i in range(n_cycles)]
+
+    # number of parallel simulations
+    n_walkers = 10
+
+    # the work mapper
+    # work_mapper = ThreadMapper()
+
+    work_mapper = Mapper()
+
+    # create the initial walkers with equal weights
+    with start_action(action_type="Init Walkers") as ctx:
+         init_weight = 1.0 / n_walkers
+         init_walkers = [Walker(copy(init_state), init_weight) for i in range(n_walkers)]
+
+    with start_action(action_type="Init Sim Manager") as ctx:
+         sim_manager = Manager(
+             init_walkers,
+             runner=runner,
+             resampler=resampler,
+             work_mapper=work_mapper)
+
+# run the simulation and get the results
+with start_action(action_type="Simulation") as ctx:
+    final_walkers, _ = sim_manager.run_simulation(n_cycles, steps)
+
+write_to_textfile("_output/metrics.prom")
--- jigs/eliot/tasks.py
+++ jigs/eliot/tasks.py
@@ -42,3 +42,95 @@ def env(cx):
 
     with cx.cd("../../"):
         cx.run(f"inv py.env-jig -n {example_name}")
+
+
+### Custom tasks
+
+
+## Prometheus
+
+PROM_VERSION = '2.18.1'
+PROM_FNAME = f"prometheus-{PROM_VERSION}.linux-amd64"
+PROM_URL = f"https://github.com/prometheus/prometheus/releases/download/v{PROM_VERSION}/{PROM_FNAME}.tar.gz"
+
+PROM_PORT = "9090"
+@task
+def prometheus_download(cx):
+
+    cx.run("mkdir -p _output/programs")
+
+    with cx.cd("_output/programs"):
+        cx.run(f"wget {PROM_URL}", pty=True)
+        cx.run(f"tar --extract -f {PROM_FNAME}.tar.gz")
+
+@task(pre=[prometheus_download])
+def prometheus_install(cx):
+
+    with cx.cd("_output/programs"):
+        cx.run(f"ln -s -r -f -T {PROM_FNAME}/prometheus prometheus")
+
+@task
+def grafana_url(cx):
+    print(f"URL: http://localhost:{PROM_PORT}")
+
+@task
+def prometheus_start(cx):
+
+    cx.run("mkdir -p _output/prometheus")
+
+    grafana_url(cx)
+
+    cx.run("./_output/programs/prometheus "
+           "--config.file=./input/prometheus.yaml "
+           "--storage.tsdb.path=./_output/prometheus/data"
+    )
+
+## grafana
+
+GRAF_VERSION = '6.7.3'
+GRAF_FNAME = f"grafana-{GRAF_VERSION}"
+GRAF_URL = f"https://dl.grafana.com/oss/release/{GRAF_FNAME}.linux-amd64.tar.gz"
+GRAF_PORT = "3000"
+
+@task
+def grafana_download(cx):
+
+    cx.run("mkdir -p _output/programs")
+
+    with cx.cd("_output/programs"):
+        cx.run(f"wget {GRAF_URL}", pty=True)
+        cx.run(f"tar --extract -f {GRAF_FNAME}.linux-amd64.tar.gz")
+
+@task(pre=[grafana_download])
+def grafana_install(cx):
+
+    with cx.cd("_output/programs"):
+        cx.run(f"ln -s -r -f -T {GRAF_FNAME} grafana")
+
+@task
+def grafana_url(cx):
+
+    print(f"URL: http://localhost:{GRAF_PORT}")
+    print("username: admin")
+    print("password: admin")
+
+@task
+def grafana_start(cx):
+
+    cx.run("mkdir -p _output/grafana")
+
+    grafana_url(cx)
+
+
+    cx.run("./_output/programs/grafana/bin/grafana-server "
+           "--homepath ./_output/programs/grafana "
+    )
+
+
+## All services
+
+@task
+def service_urls(cx):
+
+    prometheus_url(cx)
+    grafana_url(cx)
